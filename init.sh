fa02547db0c3c8a74c3304034bd7f38fc4aca22b
Sriram Venkatesh <venksriram@gmail.com>
2014-05-15 19:39:26 +1200

    Readded sync code

--- init.sh
+++ init.sh
@@ -1,4 +1,9 @@
-#!/bin/bash          
- mkdir $(date +%Y-%m-%d)
- cp init/status.tex $(date +%Y-%m-%d)/status.tex
- cp init/bib.tex $(date +%Y-%m-%d)/bib.tex
+#!/bin/bash
+cd statusReports
+if [ ! -d "$(date +%Y-%m-%d)" ]; then
+  mkdir $(date +%Y-%m-%d)
+fi
+cd ..
+ 
+ cp init/status.tex statusReports/$(date +%Y-%m-%d)/status.tex
+ cp init/bib.tex statusReports/$(date +%Y-%m-%d)/bib.tex
